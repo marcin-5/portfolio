4780debdbf37e7bfe11f6525fca6a7f35d9afba4
Marcin Bojara <marcin_5@mail.ru>
2023-09-24 11:47:27 +0200

    add basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
 import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">React - Tailwind - Portfolio</h1>
+    <div className="App">
+      <Layout>
+        <h1>my content</h1>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div>
+      <h1>Footer component</h1>
+    </div>
+  );
+}
