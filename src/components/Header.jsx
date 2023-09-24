479218a2afb82f94a17b24b1fce5de955955289b
Marcin Bojara <marcin_5@mail.ru>
2023-09-24 19:22:30 +0200

    add navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,67 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+
 export default function Header() {
+  const [showMenu, setShowMenu] = useState("md:hidden");
+  const path = window.location.pathname;
+  const menuItems = [
+    {
+      title: "Home",
+      key: "/",
+    },
+    {
+      title: "Projects",
+      key: "/projects",
+    },
+    {
+      title: "Contact",
+      key: "/contact",
+    },
+  ];
   return (
-    <div>
-      <h1>Header component</h1>
+    <div className="font-raleway text-white">
+      <div
+        className={`bg-header flex items-center justify-between p-2 shadow-lg ${
+          !showMenu && "md:flex-col"
+        }`}
+      >
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-4xl font-semibold">M B</h1>
+          <FaBars
+            onClick={() => setShowMenu(showMenu ? "" : "md:hidden")}
+            className="cursor-pointer xxl:hidden xl:hidden lg:hidden md:flex"
+          />
+        </div>
+
+        <div className="flex md:hidden">
+          {menuItems.map((item) => {
+            return (
+              <li
+                className={`mx-5 list-none px-2 py-1 ${
+                  item.key == path && "text-header rounded-md bg-white"
+                }`}
+              >
+                <Link to={item.key}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </div>
+
+        <div className={`mt-2 w-full flex-col xxl:hidden xl:hidden lg:hidden md:flex ${showMenu}`}>
+          {menuItems.map((item) => {
+            return (
+              <li
+                className={`my-2 list-none px-2 pt-1 ${
+                  item.key == path && "text-header rounded-md bg-white"
+                }`}
+              >
+                <Link to={item.key}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        header: "#202225",
+      },
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+      },
+    },
+    screens: {
+      xxl: { min: "1280px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
   },
   plugins: [],
 };
